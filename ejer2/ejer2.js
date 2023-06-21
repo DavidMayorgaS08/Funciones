ccc16df37c67b903bcaa586146b9f24c6b9a122e
DavidMayorgaS08 <david08102@hotmail.com>
2023-06-20 19:47:08 -0500

    Ejercicio 2

--- ejer2/ejer2.js
+++ ejer2/ejer2.js
@@ -2,11 +2,41 @@
 
 const edades = [];
 
+let i = 1;
+
 while(edades.length < 10){
-    const edad = parseInt(prompt("Ingrese la edad: "));
+    const edad = parseInt(prompt(`Ingrese la edad de la persona ${i}:`));
     if(edad >= 1 && edad <= 120){
         edades.push(edad);
+        i++;
     }else{
         alert("Ingrese una edad entre 1 y 120 años");
     }
 }
+
+const menores = edades.filter(edad => edad < 18);
+const mayores = edades.filter(edad => edad >= 18 && edad < 60);
+const adultosMayores = edades.filter(edad => edad >= 60);
+
+let edadMinima = edades[0];
+for(let i = 0; i < edades.length; i++){
+    if(edades[i] < edadMinima){
+        edadMinima = edades[i];
+    }
+}
+
+let edadMaxima = edades[0];
+for(let i = 0; i < edades.length; i++){
+    if(edades[i] > edadMaxima){
+        edadMaxima = edades[i];
+    }
+}
+
+const promedio = edades.reduce((a, b) => a + b, 0) / edades.length;
+
+console.log(`La cantidad de menores de edad es: ${menores.length}`);
+console.log(`La cantidad de mayores de edad es: ${mayores.length}`);
+console.log(`La cantidad de adultos mayores es: ${adultosMayores.length}`);
+console.log(`La edad mínima es: ${edadMinima}`);
+console.log(`La edad máxima es: ${edadMaxima}`);
+console.log(`El promedio de edades es: ${promedio}`);
